c8a81fb57851c4b8e60eb972a2a5ee887b90e62d
cupial <mj.cupial@gmail.com>
2023-01-05 14:27:17 +0100

    findCity func added

--- FlightSearchEngine.py
+++ FlightSearchEngine.py
@@ -2,41 +2,45 @@ import os
 import requests
 import json
 
-# fl_key = os.getenv('FLK')
-# fl_request = {
-#     'real time flights': 'flights',
-#     'flight schedules': 'advanced-flights-schedules',
-#     'airports': 'airports',
-#     'airlines': 'airlines',
-#     'countries': 'countries',
-#     'cities': 'cities'
-# }
-# city = input("the city of departure: ")
-#
-# response = requests.get(
-#     f"https://app.goflightlabs.com/{fl_request['cities']}?access_key="+fl_key)
-# data = response.json()
-# print(data)
+fl_key = os.getenv('FLK')
+fl_request = {
+    'real time flights': 'flights',
+    'flight schedules': 'advanced-flights-schedules',
+    'airports': 'airports',
+    'airlines': 'airlines',
+    'countries': 'countries',
+    'cities': 'cities'
+}
+city = input("the city of departure: ")
+
+response = requests.get(
+    f"https://app.goflightlabs.com/{fl_request['cities']}?access_key="+fl_key
+)
+data = response.json()['data']
+print(data)
+
+def findCity(city, data):
+    for elem in data:
+        if elem['nameCity'] == city:
+            print(elem)
+
+findCity(city, data)
 
 
 
+# # Opening JSON file
+# f = open('Cities.json')
 
+# # returns JSON object as
+# # a dictionary
+# data = json.load(f)
 
-
-# Opening JSON file
-f = open('Cities.json')
-
-# returns JSON object as
-# a dictionary
-data = json.load(f)
-
-# Iterating through the json
-# list
-for i in data['data']:
-    # print(i)
-    if i['nameCity'] == "Berlin":
-        print(i)
-
-# Closing file
-f.close()
+# # Iterating through the json
+# # list
+# for i in data['data']:
+#     # print(i)
+#     if i['nameCity'] == city:
+#         print(i)
+# # Closing file
+# f.close()
 
