f569e6355857a75b21e9874296365369e276de09
cupial <mj.cupial@gmail.com>
2023-02-11 13:44:51 +0100

    working on find_airport function

--- FlightSearchEngine.py
+++ FlightSearchEngine.py
@@ -12,38 +12,40 @@ fl_request = {
     'cities': 'cities'
 }
 
+TEMP_VAR = ['Paris | FR', 'Paris | US', 'Paris | US']
 
 
+# def generate_request(fl_request_key):
+#     """function for generate requests based on fl_request dictionary"""
+#     response = requests.get(
+#         f"https://app.goflightlabs.com/{fl_request[fl_request_key]}?access_key=" + fl_key
+#     )
+#     data = response.json()['data']
+#     return data
+#
+# def find_and_select_city():
+#     """Find your city as a 'start airport'"""
+#     city = input("the city of departure: ")
+#     city = city[0].upper() + city[1:].lower()
+#     print("Please wait for proccessing data...")
+#     cities = generate_request('cities')
+#     cities_list = [f"{elem['nameCity']} | {elem['codeIso2Country']}" for elem in cities if elem['nameCity'] == city]
+#     if bool(cities_list) == False:
+#         print(f"Sorry, but {city} city doesn't exists on the list")
+#     else:
+#         print(cities_list)
+#         return cities_list
 
-def generate_request(fl_request_key):
-    """function for generate requests based on fl_request dictionary"""
-    response = requests.get(
-        f"https://app.goflightlabs.com/{fl_request[fl_request_key]}?access_key=" + fl_key
-    )
-    data = response.json()['data']
-    return data
-
-def find_and_select_city():
-    """Find your city as a 'start airport'"""
-    city = input("the city of departure: ")
-    city = city[0].upper() + city[1:].lower()
-    print("Please wait for proccessing data...")
-    cities = generate_request('cities')
-    cities_list = [f"{elem['nameCity']} | {elem['codeIso2Country']}" for elem in cities if elem['nameCity'] == city]
-    if bool(cities_list) == False:
-        print(f"Sorry, but {city} city doesn't exists on the list")
-    else:
-        print(cities_list)
-        return cities_list
-
-def find_airport():
+def find_airport(cities_list):
     """Show the list of available airports for choosen city"""
-    pass
+    print(cities_list)
+    # print([i for i in cities])
 
-def check_connection(start_airport):
+
+def check_connection():
     """Check connection from your 'start airport'"""
     pass
 
-
-# print(generate_request.__doc__)
-start_airport = find_and_select_city()
+# cities_list = find_and_select_city()
+# find_airport(cities_list)
+find_airport(TEMP_VAR)
